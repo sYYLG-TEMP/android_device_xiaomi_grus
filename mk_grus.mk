9481e879f6c269f51c3cf30551b8b7984fc87124
subdragonzj <zj502407679@gmail.com>
2019-07-20 23:22:41 +0800

    grus: remove repeat inherit

--- mk_grus.mk
+++ mk_grus.mk
@@ -4,11 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-
 # Inherit from grus device
 $(call inherit-product, device/xiaomi/grus/device.mk)
 
