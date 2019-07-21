9d5512152dfdf1055625826b16603f92aeca0caa
1nfrag <sil3ntcll@gmail.com>
2019-07-20 21:53:55 -0300

    grus: Import and bind mount stock manifest
     * Stock vendor manifest from MIUI 9.7.4
    
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>
    Signed-off-by: Albert I <kras@raphielgang.org>

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
 
 # Common init scripts
 PRODUCT_PACKAGES += \
-    init.qcom.rc
+    init.qcom.rc \
+    init.target.rc
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
@@ -64,6 +65,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0_system
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/etc/manifest.xml
 
 # IFAA manager
 PRODUCT_PACKAGES += \
