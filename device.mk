28ef1106c99f810e243a850fd3c6845ca6f81881
subdragonzj <zj502407679@gmail.com>
2019-07-31 21:36:12 +0800

    grus: bind media codec from 9.7.11

--- device.mk
+++ device.mk
@@ -86,7 +86,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml \
+    $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml
 
 # Lights
 PRODUCT_PACKAGES += \
