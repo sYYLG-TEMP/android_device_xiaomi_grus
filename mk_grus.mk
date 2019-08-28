026fc03abfb20db4bc4f3e666cd999f1b230a460
subdragonzj <zj502407679@gmail.com>
2019-08-29 00:53:32 +0800

    grus: update build fingerprint V10.3.3.0

--- mk_grus.mk
+++ mk_grus.mk
@@ -17,10 +17,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9 SE
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/grus/grus:9/PKQ1.181121.001/9.7.11:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/grus/grus:9/PKQ1.181121.001/V10.3.3.0.PFBCNXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 9.7.11 release-keys" \
+    PRIVATE_BUILD_DESC="grus-user 9 PKQ1.181121.001 V10.3.3.0.PFBCNXM release-keys" \
     PRODUCT_NAME="grus" \
     TARGET_DEVICE="grus"
 
